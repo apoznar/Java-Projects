2a541ae75a1a95b9350b4737e743b3d03d466b22
apoznar <adampoznar@gmail.com>
2018-10-11 23:15:06 +0200

    Update Date.java
    Added documentation and some comments

--- Think Java excercises/2/Date.java
+++ Think Java excercises/2/Date.java
@@ -1,7 +1,18 @@
+/** 
+ * The Date class implements an application that
+ * prints desired date in American and European format.
+ *
+ * @author  Adam Poznar
+ * @version 1.1
+ * @since   2018-09-11 
+ */
 public class Date {
-
+   /**
+   * This is the main method which takes parameters from
+   * set strings and prints them in both formats.
+   * Commented lines are just for checking outputs.
+   */
     public static void main(String[] args) {
-        //this method prints desired date in American and European format
         String day = "Tuesday";
         int date = 18;
         String month = "September";
