385602e8f23f5603b097d5595ac0d97c687e3cc6
apoznar <adampoznar@gmail.com>
2018-10-13 22:18:59 +0200

    Update FermatLastTheorem.java
    Added documentation and comments

--- Think Java excercises/5/FermatLastTheorem.java
+++ Think Java excercises/5/FermatLastTheorem.java
@@ -1,11 +1,24 @@
-//this class checks Fermat's Law:
-//there are no integers a,b,c when a^n + b^n = c^n, except when n >= 2
+/** 
+ * The FermatLastTheorem class implements an application that
+ * takes four arguments and checks Fermat's Law - 
+ * there are no integers a,b,c when a^n + b^n = c^n, except when n >= 2.
+ *
+ * @author  Adam Poznar
+ * @version 1.1
+ * @since   2018-09-13
+ */
 public class FermatLastTheorem {
-    
+     /** 
+ * This main method ivokes 'checkFermat' method
+ * with four arguments.
+ */
     public static void main(String[] args) {
         checkFermat(1, 1, 1, 1);
     }
-    
+     /** 
+ * This method do if-else loop checking if set arguments from main
+ * can prove right or wrong Fermat's Law and prints appropriate message.
+ */
     public static void checkFermat(int a, int b, int c, int n) {
         if ((n > 2) && (Math.pow(a, n) + Math.pow(b, n) == Math.pow(c, n))) {
             System.out.println("Holy smokes! Fermat was wrong!"); }
