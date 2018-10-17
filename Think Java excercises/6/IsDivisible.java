313b05529459ba7e55a746ff5ab59f195446d6b7
apoznar <adampoznar@gmail.com>
2018-10-17 22:12:41 +0200

    Update IsDivisible.java
    Added documentation and comments for improving class

--- Think Java excercises/6/IsDivisible.java
+++ Think Java excercises/6/IsDivisible.java
@@ -1,9 +1,24 @@
+/**
+ * The IsDivisible class implements an application that 
+ * returns a boolean flag if conditions are met
+ * using if-else loop.
+ *
+ * @author  Adam Poznar
+ * @version 1.1
+ * @since   2018-09-13 
+ */
 public class IsDivisible {
-    
+     /** 
+ * This main method prints isDivisible method
+ * using set arguments.
+ */
     public static void main(String[] args) {
         System.out.println(isDivisible(4, 2));
     }
-    
+     /** 
+ * This isDivisible method takes two arguments from
+ * main method and checks if condition are met.
+ */
     public static boolean isDivisible(int n, int m) {
         boolean divFlag;
         if (n % m == 0) {
