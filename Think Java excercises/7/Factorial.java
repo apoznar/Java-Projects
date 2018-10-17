e313596dc90129a5504d992c7f4a66a7d6ab5727
apoznar <adampoznar@gmail.com>
2018-10-17 22:50:59 +0200

    Update Factorial.java
    Added documentation and comments for improving class

--- Think Java excercises/7/Factorial.java
+++ Think Java excercises/7/Factorial.java
@@ -1,9 +1,24 @@
+/**
+ * The Factorial class implements an application that 
+ * takes one argument from main method and makes a factorial
+ * using for loop.
+ *
+ * @author  Adam Poznar
+ * @version 1.1
+ * @since   2018-09-13
+ */
 public class Factorial {  
-    
+     /** 
+ * This main method prints factorial method
+ * using set argument.
+ */
     public static void main(String[] args) {
         System.out.println(factorial(2));
     }
-    
+     /** 
+ * This method takes argument from main method
+ * and makes a factorial from it using for loop.
+ */
     public static int factorial(int n){
         int result = 1;
         for (int i = 1; i <= n; i++){
