e417a6e576b09dca4aab5085b717356037107e8d
apoznar <adampoznar@gmail.com>
2018-10-17 23:10:33 +0200

    Update SquareRoot.java
    Added documentation and comments for class

--- Think Java excercises/7/SquareRoot.java
+++ Think Java excercises/7/SquareRoot.java
@@ -1,9 +1,25 @@
+/**
+ * The SquareRoot class implements an application that 
+ * displays "Hello World!" to the standard output
+ * and uses some escape sequences.
+ *
+ * @author  Adam Poznar
+ * @version 1.1
+ * @since   2018-09-13 
+ */
 public class SquareRoot {
-    
+     /** 
+ * This main method invokes squareRoot method 
+ * with given parameter.
+ */
     public static void main(String[] args) {
         squareRoot(2);
     }
-    
+     /** 
+ * This method takes an argument from main method
+ * and makes series of computations that are simply
+ * square root and returns value.
+ */
     public static double squareRoot(double a){
         double oldValue = a/2;
         double newValue = a/2;
