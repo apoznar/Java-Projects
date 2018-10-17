3c2cd34b174e6d3b4a387096ffa7a2f3c4c82591
apoznar <adampoznar@gmail.com>
2018-10-17 22:24:19 +0200

    Update MultAdd.java
    Added documentation and comments for improving class

--- Think Java excercises/6/MultAdd.java
+++ Think Java excercises/6/MultAdd.java
@@ -1,9 +1,16 @@
 public class MultAdd {
-    
+     /** 
+ * This main method prints Hello World output
+ * and tests newline, tab & double quote 
+ * escape sequences.
+ */
     public static void main(String[] args) {
         System.out.println(multAdd(1.0, 2.0, 0.0));
     }
-    
+     /** 
+ * This method takes three arguments and makes
+ * series of computations.
+ */
     public static double multAdd(double a, double b, double c) {
         double multAddFlag = a * b + c;
         double firstCompute = (Math.sin(Math.PI/4.0)) 
@@ -14,6 +21,10 @@ public class MultAdd {
         expSum(2.0);
         return multAddFlag;
     }
+     /** 
+ * This method computes expotential value of variable x
+ * and returns it.
+ */
     public static double expSum(double x) {
         double expCompute = x * Math.exp(-x) + Math.sqrt(1 - Math.exp(-x));
         return expCompute;
