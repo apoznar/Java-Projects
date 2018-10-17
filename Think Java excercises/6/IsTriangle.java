289852dec663e4fdff94a103fe285c1e0c9a0652
apoznar <adampoznar@gmail.com>
2018-10-17 22:18:50 +0200

    Update IsTriangle.java
    Added documentation and comments for improving class

--- Think Java excercises/6/IsTriangle.java
+++ Think Java excercises/6/IsTriangle.java
@@ -1,9 +1,25 @@
+/**
+ * The IsTriangle class implements an application that 
+ * takes three arguments from main method
+ * and prints variable with certain boolean value.
+ *
+ * @author  Adam Poznar
+ * @version 1.1
+ * @since   2018-09-13 
+ */
 public class IsTriangle {
-    
+     /** 
+ * This main method takes three arguments and
+ * prints isTriangle output.
+ */
     public static void main(String[] args) {
         System.out.println(isTriangle(2, 2, 4));
     }
-    
+     /** 
+ * This isTriangle method uses if-else loop
+ * and changes boolean value of a variable if
+ * certain conditions are met.
+ */
     public static boolean isTriangle(int x, int y, int z) {
         boolean triangleFlag;
         if (x + y <= z || z + y <= x || x + z <= y) {
