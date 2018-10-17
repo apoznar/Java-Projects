134ee53099eb0a46850e258fc3f03b92e27dd1b2
apoznar <adampoznar@gmail.com>
2018-10-17 22:55:03 +0200

    Update Gauss.java
    Added documentation and comments for class

--- Think Java excercises/7/Gauss.java
+++ Think Java excercises/7/Gauss.java
@@ -1,9 +1,23 @@
+/**
+ * The Gauss class implements an application that 
+ * takes two arguments and computes Gauss equation.
+ *
+ * @author  Adam Poznar
+ * @version 1.1
+ * @since   2018-09-13 
+ */
 public class Gauss {
-    
+     /** 
+ * This main method takes two arguments
+ * and print gauss method output.
+ */
     public static void main(String[] args) {
         System.out.println(gauss(1,1));
     }
-    
+     /** 
+ * This method takes two arguments from main method
+ * and coputes Gauss equation using for loop.
+ */
     public static double gauss(double x, int n){
         double result = 1;
         int q = 1;
