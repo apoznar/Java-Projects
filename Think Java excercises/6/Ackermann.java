4673552fe8fcbe0aeabdbebd187b8e588eac8f2c
apoznar <adampoznar@gmail.com>
2018-10-17 22:03:24 +0200

    Update Ackermann.java
    Added documentation and comments for improving class

--- Think Java excercises/6/Ackermann.java
+++ Think Java excercises/6/Ackermann.java
@@ -1,9 +1,25 @@
+/**
+ * The Ackermann class implements an application that 
+ *  calculate the Ackermann function A(M, N)
+ *  using a straightforward recursive program.
+ *
+ * @author  Adam Poznar
+ * @version 1.1
+ * @since   2018-09-13 
+ */
 public class Ackermann {
-    
+     /** 
+ * This main method prints prints
+ * 'ack' method with two arguments.
+ */
     public static void main(String[] args) {
         System.out.println(ack(0, 2));
     }
-    
+     /** 
+ * This method takes two integer arguments
+ * and returns proper value through if-else loop
+ * when conditions met.
+ */
     public static int ack(int m, int n) {
         if (m == 0) {
             return n + 1;
