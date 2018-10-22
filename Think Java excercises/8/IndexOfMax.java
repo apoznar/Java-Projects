70770607ab6d14b0db6173a3d257a72e14892095
apoznar <adampoznar@gmail.com>
2018-10-22 20:49:16 +0200

    Update IndexOfMax.java
    Added documentation and comments for class

--- Think Java excercises/8/IndexOfMax.java
+++ Think Java excercises/8/IndexOfMax.java
@@ -1,8 +1,19 @@
+/**
+ * The IndexOfMax class implements an application that 
+ *
+ * @author  Adam Poznar
+ * @version 1.1
+ * @since   2018-09-14 
+ */
 public class IndexOfMax {
-    
+     /** 
+ * This main method
+ */
     public static void main(String[] args) {
     }
-    
+     /** 
+ * This method
+ */
     public static int indexOfMax(int[] numbers){
         int index = 0;
         for (int i=1;i<numbers.length;i++){
@@ -12,7 +23,9 @@ public class IndexOfMax {
         }
         return index;
     }
-    
+     /** 
+ * This method
+ */
     public static int indexOfMaxWrittenInEnhancedForLoop(int[] numbers){
         int index = 0;
         int i = 0;
