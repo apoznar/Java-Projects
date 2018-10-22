dbfcdc291d93bf590514d982f18b5a311c9f58b1
apoznar <adampoznar@gmail.com>
2018-10-22 20:50:52 +0200

    Update MaxInRange.java

--- Think Java excercises/8/MaxInRange.java
+++ Think Java excercises/8/MaxInRange.java
@@ -1,8 +1,19 @@
+/**
+ * The MaxInRange class implements an application that 
+ *
+ * @author  Adam Poznar
+ * @version 1.1
+ * @since   2018-09-14 
+ */
 public class MaxInRange {
-    
+     /** 
+ * This main method 
+ */
     public static void main(String[] args) {
     }
-    
+     /** 
+ * This method 
+ */
     public static int maxInRange(int[] integers, int lowIndex, int highIndex){
         if (lowIndex == highIndex){
             return integers[lowIndex];
@@ -10,7 +21,9 @@ public class MaxInRange {
         int mid = (lowIndex + highIndex) / 2;
         return Math.max(maxInRange(integers,lowIndex,mid),maxInRange(integers,(mid + 1),highIndex));
     }
-    
+     /** 
+ * This method 
+ */
     public static int max(int[] integers){
         return maxInRange(integers, 0, integers.length - 1);
     }
