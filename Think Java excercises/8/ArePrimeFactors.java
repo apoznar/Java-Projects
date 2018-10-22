86dd7bb7d940d505867a907bf1b2fb347cde97e4
apoznar <adampoznar@gmail.com>
2018-10-22 20:45:14 +0200

    Update ArePrimeFactors.java
    Added documentation and comments for class

--- Think Java excercises/8/ArePrimeFactors.java
+++ Think Java excercises/8/ArePrimeFactors.java
@@ -1,9 +1,23 @@
+/**
+ * The Hello class implements an application that 
+ * displays "Hello World!" to the standard output
+ * and uses some escape sequences.
+ *
+ * @author  Adam Poznar
+ * @version 1.1
+ * @since   2018-09-11 
+ */
 public class ArePrimeFactors {
-    
+     /** 
+ * This main method executes both methods.
+ */
     public static void main(String[] args) {
     }
     
-    
+     /** 
+ * This method checks if n is > 2, then chcecks if
+* it is even.
+ */
     public static boolean isPrime(int n){
         if (n < 2){
             return false;
@@ -21,7 +35,9 @@ public class ArePrimeFactors {
         }
         return true;
     }
-    
+     /** 
+ * This method checks if elements in an array are..
+ */
     public static boolean arePrimeFactors(int n, int[] arr){
         int result = 1;
         for (int element:arr){
