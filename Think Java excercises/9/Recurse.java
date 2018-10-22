f1e7b5d21c7c84f217496c06709cfb95ddb1415f
apoznar <adampoznar@gmail.com>
2018-10-22 21:49:22 +0200

    Update Recurse.java
    Added documentation and comments for class

--- Think Java excercises/9/Recurse.java
+++ Think Java excercises/9/Recurse.java
@@ -1,30 +1,49 @@
+/**
+ * The Recurse class implements an application that 
+ * displays "Hello World!" to the standard output
+ * and uses some escape sequences.
+ *
+ * @author  Adam Poznar
+ * @version 1.1
+ * @since   2018-09-14
+ */
 public class Recurse {
-    
+     /** 
+ * This main method 
+ */
     public static void main(String args[]){
         System.out.println(isPalindrome("otto"));
     }
-    
+     /** 
+ * This main method 
+ */
     public static void printString(String str){
         if (length(str) != 0){
             System.out.println(first(str));
             printString(rest(str));
         }
     }
-    
+     /** 
+ * This main method 
+ */
     public static void printBackward(String str){
         if (length(str) != 0){
             printBackward(rest(str));
             System.out.println(first(str));
         }
     }
-    
+     /** 
+ * This main method 
+ */
     public static String reverseString(String str){
         if (length(str) != 0){
             return reverseString(rest(str)) + first(str);
         }
         return "";
     }
-    
+     /** 
+ * This main method 
+ */
     public static boolean isPalindrome(String str){
         int length = length(str);
         if (length == 1){
@@ -37,33 +56,4 @@ public class Recurse {
             return first(str) == first(reverseString(rest(str))) && isPalindrome(middle(str));
         }
     }
-    // MARK: Supporting
     
-    /**
-     * Returns the first character of the given String.
-     */
-    public static char first(String s){
-        return s.charAt(0);
-    }
-    
-    /**
-     * Returns all but the first letter of the given String.
-     */
-    public static String rest(String s) {
-        return s.substring(1);
-    }
-    
-    /**
-     * Returns all but the first and last letter of the String.
-     */
-    public static String middle(String s) {
-        return s.substring(1, s.length() - 1);
-    }
-    
-    /**
-     * Returns the length of the given String.
-     */
-    public static int length(String s) {
-        return s.length();
-    }
-}
