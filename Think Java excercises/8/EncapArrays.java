5556b822151605bdb7215af45abe6a535fe7445b
apoznar <adampoznar@gmail.com>
2018-10-22 20:47:12 +0200

    Update EncapArrays.java
    Added documentation and comments for class

--- Think Java excercises/8/EncapArrays.java
+++ Think Java excercises/8/EncapArrays.java
@@ -1,8 +1,21 @@
+/**
+ * The EncapArrays class implements an application that 
+ * 
+ * 
+ *
+ * @author  Adam Poznar
+ * @version 1.1
+ * @since   2018-09-14 
+ */
 public class EncapArrays {
-    
+     /** 
+ * This main method 
+ */
     public static void main(String[] args) {
     }
-    
+     /** 
+ * This method 
+ */
     public static double[] powArray(double[] array, double power){
         double[] accumulator = new double[array.length];
         for (int i=0;i<array.length;i++){
@@ -10,7 +23,9 @@ public class EncapArrays {
         }
         return accumulator;
     }
-    
+     /** 
+ * This method 
+ */
     public static int[] histogram(int[] array, int count){
         int[] counter = new int[count];
         for (int element:array){
