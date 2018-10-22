db5d314d3f796e7d519461aab340a63b4e5092ad
apoznar <adampoznar@gmail.com>
2018-10-22 20:51:38 +0200

    Update Sieve.java
    Added documentation and comments for class

--- Think Java excercises/8/Sieve.java
+++ Think Java excercises/8/Sieve.java
@@ -1,8 +1,25 @@
+/**
+ * The Hello class implements an application that 
+ * displays "Hello World!" to the standard output
+ * and uses some escape sequences.
+ *
+ * @author  Adam Poznar
+ * @version 1.1
+ * @since   2018-09-11 
+ */
 public class Sieve {
-    
+     /** 
+ * This main method prints Hello World output
+ * and tests newline, tab & double quote 
+ * escape sequences.
+ */
     public static void main(String[] args) {
     }
-    
+     /** 
+ * This main method prints Hello World output
+ * and tests newline, tab & double quote 
+ * escape sequences.
+ */
     public static boolean[] sieve(int n){
         boolean[] result = new boolean[n];
         for (int i=2;i<n;i++)
