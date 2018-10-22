c164f1a85469741029f1805e9ab4c67ef9d3dfb0
apoznar <adampoznar@gmail.com>
2018-10-22 21:53:40 +0200

    Update Diff.java
    Added documentation and comments for class

--- Think Java excercises/9/Diff.java
+++ Think Java excercises/9/Diff.java
@@ -1,5 +1,16 @@
+    /**
+ * The Hello class implements an application that 
+ * displays "Hello World!" to the standard output
+ * and uses some escape sequences.
+ *
+ * @author  Adam Poznar
+ * @version 1.1
+ * @since   2018-09-11 
+ */
 public class Diff {
-    
+     /** 
+ * This main method 
+ */
     public static int diff(String str, char open, char close){
         int count = 0;
         for (int i = 0; i<str.length(); i++){
@@ -13,7 +24,9 @@ public class Diff {
         }
         return count;
     }
-    
+     /** 
+ * This main method 
+ */
     public static int diffParenthesis(String str){
         return diff(str, '(',')');
     }
