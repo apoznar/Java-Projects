423b875867172d70d90b7487a109be5b201609bb
apoznar <adampoznar@gmail.com>
2018-10-22 20:39:19 +0200

    Update AreFactors.java
    Added documentation and comments for class

--- Think Java excercises/8/AreFactors.java
+++ Think Java excercises/8/AreFactors.java
@@ -1,8 +1,24 @@
+/**
+ * The AreFactors class implements an application that 
+ * displays "Hello World!" to the standard output
+ * and uses some escape sequences.
+ *
+ * @author  Adam Poznar
+ * @version 1.1
+ * @since   2018-09-14 
+ */
+
 public class AreFactors {
-    
+     /** 
+ * This main method executes areFactors method.
+ */
     public static void main(String[] args) {
     }
-    
+     /** 
+ * This method checks if an desired integer factor
+ * is also in desired array and returns false if not
+ * by using for loop.
+ */
     
     public static boolean areFactors(int n, int[] factors){
         for (int factor:factors){
