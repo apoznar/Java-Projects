eba60c3c64a47d17cf4af6ea9c630b6ccc140c4c
apoznar <adampoznar@gmail.com>
2018-10-22 21:52:45 +0200

    Update IsAbecedarian.java
    Added documentation and comments for class

--- Think Java excercises/9/IsAbecedarian.java
+++ Think Java excercises/9/IsAbecedarian.java
@@ -1,5 +1,16 @@
+/**
+ * The Hello class implements an application that 
+ * displays "Hello World!" to the standard output
+ * and uses some escape sequences.
+ *
+ * @author  Adam Poznar
+ * @version 1.1
+ * @since   2018-09-14 
+ */
 public class IsAbecedarian {
-    
+     /** 
+ * This main method 
+ */
     public static boolean isAbecedarian(String str){
         String converted = str.toUpperCase();
         for (int i = 0;i<converted.length() - 1;i++){
