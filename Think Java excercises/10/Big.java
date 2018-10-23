ea923bfdc6d4d0bc0ab55b5fb34b093df2417501
apoznar <adampoznar@gmail.com>
2018-10-23 18:51:59 +0200

    Update Big.java
    Added documentation and comments for class

--- Think Java excercises/10/Big.java
+++ Think Java excercises/10/Big.java
@@ -1,6 +1,17 @@
 import java.math.BigInteger;
-
+/**
+ * The Hello class implements an application that 
+ * displays "Hello World!" to the standard output
+ * and uses some escape sequences.
+ *
+ * @author  Adam Poznar
+ * @version 1.1
+ * @since   2018-09-15
+ */
 public class Big{
+     /** 
+ * This main method 
+ */
     public static void main(String args[]){
         BigInteger integer = BigInteger.valueOf(2147483647);
         System.out.println(integer.add(BigInteger.ONE).pow(10));
@@ -8,6 +19,9 @@ public class Big{
             System.out.println(i+": "+factorial(i));
         }
     }
+     /** 
+ * This main method 
+ */
     public static BigInteger factorial(int number){
         BigInteger result = BigInteger.valueOf(1);
         for (int i = number;i>0;i--){
@@ -15,5 +29,4 @@ public class Big{
         }
         return result; 
     }
-    // Imutable; According to documentation...; No Value to change
 }
