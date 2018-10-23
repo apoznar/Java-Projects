e99fac132c122cac42eda2a387ebbf8c528e3928
apoznar <adampoznar@gmail.com>
2018-10-23 18:59:08 +0200

    Update Date.java
    Added documentation and comments for class

--- Think Java excercises/11/Date.java
+++ Think Java excercises/11/Date.java
@@ -1,4 +1,16 @@
+/**
+ * The Hello class implements an application that 
+ * displays "Hello World!" to the standard output
+ * and uses some escape sequences.
+ *
+ * @author  Adam Poznar
+ * @version 1.1
+ * @since   2018-09-15 
+ */
 public class Date{
+    /** 
+ * This main method 
+ */
    public static void main(String args[]){
       Date birthday = new Date();
       System.out.println(birthday.year+" "+birthday.month+" "+birthday.day);
