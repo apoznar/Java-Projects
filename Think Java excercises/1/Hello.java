5c4cbe181bd84a0ae4cf72697abd507efb274bf3
apoznar <adampoznar@gmail.com>
2018-10-24 15:04:27 +0200

    Update Hello.java
    Tidying up the code

--- Think Java excercises/1/Hello.java
+++ Think Java excercises/1/Hello.java
@@ -1,19 +1,18 @@
 /** 
  * The Hello class implements an application that
- * simply displays "Hello World!" to the standard output
- * and uses some escape sequences.
+ * simply displays "Hello World!" to the standard 
+ * output and uses some escape sequences.
  *
  * @author  Adam Poznar
- * @version 1.1
+ * @version 1.2
  * @since   2018-09-11 
  */
 
 public class Hello {
- /** 
- * This main method prints Hello World output
- * and tests newline, tab & double quote 
- * escape sequences.
- */
+    /** 
+     * prints Hello World output and tests
+     * newline, tab & double quote escape sequences.
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("How are you?\n\tDo you need some \"magic\" coffee?");
