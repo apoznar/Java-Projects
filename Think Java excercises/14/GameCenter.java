be0ba7990817c0fe1dd5af7029157553093dc224
apoznar <adampoznar@gmail.com>
2018-10-24 13:22:01 +0200

    Update GameCenter.java
    Added documentation and comments for class

--- Think Java excercises/14/GameCenter.java
+++ Think Java excercises/14/GameCenter.java
@@ -1,6 +1,17 @@
 import java.util.ArrayList;
-
+/**
+ * The Hello class implements an application that 
+ * displays "Hello World!" to the standard output
+ * and uses some escape sequences.
+ *
+ * @author  Adam Poznar
+ * @version 1.1
+ * @since   2018-09-15 
+ */
 public class GameCenter{
+    /** 
+ * This main method 
+ */
    public static void main(String args[]){
       Eights.debug = true;
       int count = 20;
