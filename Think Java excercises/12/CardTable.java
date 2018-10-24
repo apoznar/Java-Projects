4e3eca4c8381d9f3085e136bba17c0760abdf20b
apoznar <adampoznar@gmail.com>
2018-10-24 13:05:17 +0200

    Update CardTable.java
    Added documentation and comments for class

--- Think Java excercises/12/CardTable.java
+++ Think Java excercises/12/CardTable.java
@@ -5,7 +5,15 @@ import java.awt.Image;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-
+/**
+ * The Hello class implements an application that 
+ * displays "Hello World!" to the standard output
+ * and uses some escape sequences.
+ *
+ * @author  Adam Poznar
+ * @version 1.1
+ * @since   2018-09-15 
+ */
 public class CardTable extends Canvas {
 
     private Image[][] images;
