c6f0cf6726005ccab3121625cca05e91c0555ffd
apoznar <adampoznar@gmail.com>
2018-10-24 13:11:54 +0200

    Update MickeyMouse.java
    Added documentation and comments for class

--- Think Java excercises/Appendix B/MickeyMouse.java
+++ Think Java excercises/Appendix B/MickeyMouse.java
@@ -1,6 +1,14 @@
 import java.awt.*;
 import javax.swing.*;
-
+/**
+ * The Hello class implements an application that 
+ * displays "Hello World!" to the standard output
+ * and uses some escape sequences.
+ *
+ * @author  Adam Poznar
+ * @version 1.1
+ * @since   2018-09-11 
+ */
 public class MickeyMouse extends Canvas{
    public static void main(String[] args){
       JFrame frame = new JFrame("Window Title");
@@ -10,6 +18,9 @@ public class MickeyMouse extends Canvas{
       frame.pack();
       frame.setVisible(true);
    }
+    /** 
+ * This main method 
+ */
    public void paint(Graphics g){
       // Drawing codes
       Color ty = new Color(102,204,255);
@@ -17,9 +28,15 @@ public class MickeyMouse extends Canvas{
       Rectangle r = new Rectangle(152,304,96,96);
       mickey(g, r);
    }
+    /** 
+ * This main method 
+ */
    public void boxOval(Graphics g, Rectangle bb) {
       g.fillOval(bb.x, bb.y, bb.width, bb.height);
    }
+    /** 
+ * This main method 
+ */
    public void mickey(Graphics g, Rectangle bb) {
       boxOval(g, bb);
       int dx = bb.width / 2;
