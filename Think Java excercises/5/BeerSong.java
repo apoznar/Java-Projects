ebb8881097e512f2c274aecb13e7b796998c603f
apoznar <adampoznar@gmail.com>
2018-10-25 17:10:21 +0200

    Update BeerSong.java
    Tidying up the code

--- Think Java excercises/5/BeerSong.java
+++ Think Java excercises/5/BeerSong.java
@@ -3,23 +3,22 @@
  * displays bottle song by using if-else operator.
  *
  * @author  Adam Poznar
- * @version 1.1
- * @since   2018-09-13 
+ * @version 1.2
+ * @since   2018-09-15 
  */
 public class BeerSong {
-     /** 
- * This main method sets starting number of bottles as x
- * and ivokes 'bottles' method. 
- */
+    /** 
+     * This main method sets starting number of bottles as x
+     * and invokes 'bottles' method. 
+     */
     public static void main(String[] args) {
         int x = 99;
         bottles(x);
     }
- /** 
- * This method prints song verses and decrement x until
- * it reaches 0. Then it prints set of statements and ends.
- * escape sequences.
- */
+    /** 
+     * Prints song verses and decrement x variable using else branch until
+     * it reaches 0. Then it prints a set of statements and prints blank line.
+     */
     public static void bottles(int x) {
         if (x == 0) {
             System.out.println("No bottles of the beer on the wall,\n" +
