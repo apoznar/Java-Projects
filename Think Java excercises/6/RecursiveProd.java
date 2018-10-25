cc251a5cda0fbbcf9e48f3e9e7ad0010f5b26e5c
apoznar <adampoznar@gmail.com>
2018-10-25 18:07:35 +0200

    Update RecursiveProd.java
    Tidying up the code

--- Think Java excercises/6/RecursiveProd.java
+++ Think Java excercises/6/RecursiveProd.java
@@ -3,26 +3,20 @@
  * returns appropriate output using given arguments
  *
  * @author  Adam Poznar
- * @version 1.1
- * @since   2018-09-13 
+ * @version 1.2
+ * @since   2018-09-16 
  */
 public class RecursiveProd {
-     /** 
- * This main method gives two arguments and prints
- * appropriate output.
- */
+    /** 
+     * This main method gives two arguments and prints 'prod' method output.
+     */
     public static void main(String[] args) {
         System.out.println(prod(1, 4));
     }
-     /** 
- * This method takes arguments from main method,
- * computes equation and returns value.
- */
+    /** 
+     * Takes arguments from main method, computes equation and returns value.
+     */
     public static int prod(int m, int n) {
-        return ((m==n)?(n):(n*prod(m, n - 1)));
+        return ((m==n) ? (n) : (n*prod(m, n - 1)));
     }
 }
-
-
-
-
