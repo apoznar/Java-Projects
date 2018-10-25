2d5ebd67b58e90958b342aaa0e1cac17d1545dd9
apoznar <adampoznar@gmail.com>
2018-10-25 17:59:53 +0200

    Update OddSum.java
    Tidying up the code

--- Think Java excercises/6/OddSum.java
+++ Think Java excercises/6/OddSum.java
@@ -4,28 +4,26 @@
  * appropriate sum using if-else loop.
  *
  * @author  Adam Poznar
- * @version 1.1
- * @since   2018-09-13 
+ * @version 1.2
+ * @since   2018-09-16 
  */
 public class OddSum {
-     /** 
- * This main method prints oddSum method
- * with set argument.
- */
+    /** 
+     * This main method prints oddSum method with set argument.
+     */
     public static void main(String[] args) {
         System.out.println(oddSum(5));
     }
-     /** 
- * This method takes argument from main method
- * and returns appropriate value using if-else loop.
- */
+    /** 
+     * Takes argument from main method 
+     * and returns appropriate value using if-else loop.
+     */
     public static int oddSum(int n) {
         if (n % 2 != 0 && n > 1) {
-            return (n + oddSum(n - 2)); }
+            return (n + oddSum(n - 2)); 
+        }
         else {
             return 1;
         }
-        
     }
 }
-
