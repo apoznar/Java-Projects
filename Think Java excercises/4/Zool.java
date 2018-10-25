4bc8dae2163be63c13b8c724727a3cf4e7774924
apoznar <adampoznar@gmail.com>
2018-10-25 17:04:29 +0200

    Update Zool.java
    Tidying up the code

--- Think Java excercises/4/Zool.java
+++ Think Java excercises/4/Zool.java
@@ -3,27 +3,27 @@
  * prints variables by using other method than main.
  *
  * @author  Adam Poznar
- * @version 1.1
- * @since   2018-09-12 
+ * @version 1.2
+ * @since   2018-09-14 
  */
 public class Zool {
- /** 
- * This method takes three variables from main method
- * and prints them in separate lines.
- */
-    public static void zool(int x, String y, String z) {
-        System.out.println(x);
-        System.out.println(y);
-        System.out.println(z);
-    }
- /** 
- * This main method contains three variables
- * and invokes other method with them.
- */
+    /** 
+     * This main method contains three variables
+     * and invokes other method with them.
+     */
     public static void main(String[] args) {
         int x = 11;
         String y = "Cat";
         String z = "Jaworowa";
         zool(x, y, z);
+    }
+    /** 
+     * This method takes three variables from main method
+     * and prints them in separate lines.
+     */
+    public static void zool(int x, String y, String z) {
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
     }
 }
