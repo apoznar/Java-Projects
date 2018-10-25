aa6de765228346e09345a1debc2e687dc5e81574
apoznar <adampoznar@gmail.com>
2018-10-25 17:02:43 +0200

    Update EncapsulatedDate.java
    Tidying up the code

--- Think Java excercises/4/EncapsulatedDate.java
+++ Think Java excercises/4/EncapsulatedDate.java
@@ -3,30 +3,14 @@
  * displays set date in different formats using multiple methods.
  *
  * @author  Adam Poznar
- * @version 1.1
- * @since   2018-09-12 
+ * @version 1.2
+ * @since   2018-09-14 
  */
 public class EncapsulatedDate {
- /** 
- * This method takes four variables from main method
- * and prints them in American format.
- */
-    public static void printAmerican(String day, int date, String month, int year) {
-        System.out.println("American format: ");
-        System.out.println(day + ", " + month + " " + date + ", " + year);
-    }
- /** 
- * This method takes four variables from main method
- * and prints them in European format.
- */
-    public static void printEuropean(String day, int date, String month, int year) {
-        System.out.println("European format: ");
-        System.out.println(day + " " + date + " " + month + " " + year);
-    }
- /** 
- * This main method contains four variables as a date
- * and invokes two other methods with them.
- */
+    /** 
+     * This main method contains four variables as a date
+     * and invokes two other methods with them.
+     */
     public static void main(String[] args) {
         String day = "Tuesday";
         int date = 18;
@@ -35,8 +19,18 @@ public class EncapsulatedDate {
         printAmerican(day, date, month, year);
         printEuropean(day, date, month, year);
     }
+    /** 
+     * Takes four variables from main method and prints them in American format.
+     */
+    public static void printAmerican(String day, int date, String month, int year) {
+        System.out.println("American format: ");
+        System.out.println(day + ", " + month + " " + date + ", " + year);
+    }
+    /** 
+     * Takes four variables from main method and prints them in European format.
+     */
+    public static void printEuropean(String day, int date, String month, int year) {
+        System.out.println("European format: ");
+        System.out.println(day + " " + date + " " + month + " " + year);
+    }
 }
-
-
-
-       
