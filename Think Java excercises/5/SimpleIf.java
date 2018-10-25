e1606948ce6979fe0bfde974551fb4b0d7821613
apoznar <adampoznar@gmail.com>
2018-10-25 17:23:03 +0200

    Update SimpleIf.java
    Tidying up the code

--- Think Java excercises/5/SimpleIf.java
+++ Think Java excercises/5/SimpleIf.java
@@ -1,16 +1,16 @@
 /** 
  * The Simple class implements an application that
- * checks if number under variable x is positive digit number.
+ * checks if number in variable x is positive digit number.
  *
  * @author  Adam Poznar
- * @version 1.1
- * @since   2018-09-13
+ * @version 1.2
+ * @since   2018-09-15
  */
 public class SimpleIf {
-     /** 
- * This main method sets variable x and checks if it's
- * positive single digit number.
- */
+    /** 
+     * This main method sets variable x and checks if it is
+     * a positive single digit number.
+     */
     public static void main(String[] args) {
         int x = 5;
         if (x > 0 && x < 10) {
