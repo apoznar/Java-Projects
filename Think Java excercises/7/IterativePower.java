e09f017622b564b97af450f2c9b3876da22e680a
apoznar <adampoznar@gmail.com>
2018-10-26 14:54:25 +0200

    Update IterativePower.java
    Tidying up the code

--- Think Java excercises/7/IterativePower.java
+++ Think Java excercises/7/IterativePower.java
@@ -4,29 +4,26 @@
  * using for loop.
  *
  * @author  Adam Poznar
- * @version 1.1
- * @since   2018-09-13 
+ * @version 1.2
+ * @since   2018-09-17 
  */
 public class IterativePower {
-     /** 
- * This main method gives two arguments
- * and prints output of a power method.
- */
+    /** 
+     * This main method gives two arguments
+     * and prints output of a power method.
+     */
     public static void main(String[] args) {
-        System.out.println(power(0.0, 2));
+        System.out.println(power(2, 2));
     }
-     /** 
- * This method takes two arguments from main method
- * and modifies result variable using for loop. 
- */
-    public static double power(double x, int n){
+    /** 
+     * Takes two arguments from main and modifies result variable using for loop. 
+     * 'Result' variable is multiplied by x by n times and then prints variable.
+     */
+    public static double power(double x, int n) {
         double result = 1.0;
-        for (int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             result *= x;
         }
         return result;
     }
 }
-
-
-     
