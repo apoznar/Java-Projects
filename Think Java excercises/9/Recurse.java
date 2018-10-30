60154378a46b4df1f05ba6654a6ae06ce530f201
apoznar <adampoznar@gmail.com>
2018-10-30 18:29:45 +0100

    Update Recurse.java
    Tidying up the code and comments

--- Think Java excercises/9/Recurse.java
+++ Think Java excercises/9/Recurse.java
@@ -4,56 +4,89 @@
  * and uses some escape sequences.
  *
  * @author  Adam Poznar
- * @version 1.1
- * @since   2018-09-14
+ * @version 1.2
+ * @since   2018-09-19
  */
 public class Recurse {
-     /** 
- * This main method 
- */
-    public static void main(String args[]){
-        System.out.println(isPalindrome("otto"));
+    /** 
+     * This main method sets word String and invokes four methods with it
+     * as an parameter.
+     */
+    public static void main(String args[]) {
+        String word = "ada";
+        printString(word);
+        printBackward(word);
+        System.out.println(reverseString(word));
+        System.out.println(isPalindrome(word));
     }
-     /** 
- * This main method 
- */
-    public static void printString(String str){
-        if (length(str) != 0){
+    
+    /** 
+     * Takes str string as a parameter and displays letters of the string,
+     * one on each line.
+     */
+    public static void printString(String str) {
+        if (length(str) != 0) {
             System.out.println(first(str));
             printString(rest(str));
         }
     }
-     /** 
- * This main method 
- */
-    public static void printBackward(String str){
-        if (length(str) != 0){
+    /** 
+     * Takes str string as a parameter and displays letters of the string,
+     * one on each line, but backwards.
+     */
+    public static void printBackward(String str) {
+        if (length(str) != 0) {
             printBackward(rest(str));
             System.out.println(first(str));
         }
     }
-     /** 
- * This main method 
- */
-    public static String reverseString(String str){
-        if (length(str) != 0){
+    /** 
+     * Takes str string as a parameter and returns a new string as a return value.
+     */
+    public static String reverseString(String str) {
+        if (length(str) != 0) {
             return reverseString(rest(str)) + first(str);
         }
         return "";
     }
-     /** 
- * This main method 
- */
-    public static boolean isPalindrome(String str){
+    /** 
+     * Takes str string and checks if it is an palindrome, then returns true.
+     */
+    public static boolean isPalindrome(String str) {
         int length = length(str);
-        if (length == 1){
+        if (length == 1) {
             return true;
         } 
-        else if (length == 2){
+        else if (length == 2) {
             return first(str) == first(rest(str));
         } 
         else {
-            return first(str) == first(reverseString(rest(str))) && isPalindrome(middle(str));
+            return first(str) == first(reverseString(rest(str)))
+                && isPalindrome(middle(str));
         }
     }
-    
+    /**
+     * Returns the first character of the given String.
+     */
+    public static char first(String s) {
+        return s.charAt(0);
+    } 
+    /**
+     * Returns all but the first letter of the given String.
+     */
+    public static String rest(String s) {
+        return s.substring(1);
+    }  
+    /**
+     * Returns all but the first and last letter of the String.
+     */
+    public static String middle(String s) {
+        return s.substring(1, s.length() - 1);
+    }
+    /**
+     * Returns the length of the given String.
+     */
+    public static int length(String s) {
+        return s.length();
+    }
+}
