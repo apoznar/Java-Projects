b7a21ad25e7dfbacb9306578ab63d333386d83c5
apoznar <adampoznar@gmail.com>
2018-10-30 14:49:41 +0100

    Update AreFactors.java
    minor comment and code tweaks

--- Think Java excercises/8/AreFactors.java
+++ Think Java excercises/8/AreFactors.java
@@ -1,27 +1,27 @@
 /**
  * The AreFactors class implements an application that 
- * displays "Hello World!" to the standard output
- * and uses some escape sequences.
+ * takes an integer n and an array of integers, an that 
+ * returns true if the numbers in the array are all factors of n.
  *
  * @author  Adam Poznar
- * @version 1.1
- * @since   2018-09-14 
+ * @version 1.2
+ * @since   2018-09-18 
  */
-
 public class AreFactors {
-     /** 
- * This main method executes areFactors method.
- */
-    public static void main(String[] args) {
+    /** 
+     * This main method executes areFactors method.
+     */
+    public static void main(String args[]) {
+        int[] a = {1, 8};
+        System.out.println(areFactors(8, a));
     }
-     /** 
- * This method checks if an desired integer factor
- * is also in desired array and returns false if not
- * by using for loop.
- */
-    
-    public static boolean areFactors(int n, int[] factors){
-        for (int factor:factors){
+    /** 
+     * This method checks if an desired integer factor
+     * is also in desired array and returns false if not
+     * by using for loop.
+     */
+    public static boolean areFactors(int n, int[] factors) {
+        for (int factor : factors) {
             if (n % factor != 0)
                 return false;
         }
