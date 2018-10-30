d76961282eca0db43935e940cbfd9de467bb6605
apoznar <adampoznar@gmail.com>
2018-10-30 14:43:43 +0100

    Update Sieve.java
    Changed whole code after testing

--- Think Java excercises/8/Sieve.java
+++ Think Java excercises/8/Sieve.java
@@ -1,36 +1,49 @@
 /**
- * The Hello class implements an application that 
- * displays "Hello World!" to the standard output
- * and uses some escape sequences.
+ * The Sieve class implements an application that 
+ * is a simple, ancient algorithm for finding all 
+ * prime number up to any given limit.
  *
  * @author  Adam Poznar
- * @version 1.1
- * @since   2018-09-11 
+ * @version 1.2
+ * @since   2018-09-18 
  */
-public class Sieve {
-     /** 
- * This main method prints Hello World output
- * and tests newline, tab & double quote 
- * escape sequences.
- */
-    public static void main(String[] args) {
-    }
-     /** 
- * This main method prints Hello World output
- * and tests newline, tab & double quote 
- * escape sequences.
- */
-    public static boolean[] sieve(int n){
-        boolean[] result = new boolean[n];
-        for (int i=2;i<n;i++)
-            result[i] = true;
-        for (int number=2;number<=(int)Math.sqrt(n);number++){
-            if (result[number] == true){
-                for (int indexInSeries = number*number;indexInSeries < n;indexInSeries += number){
-                    result[indexInSeries] = false;
-                }
-            }
-        }
-        return result;
-    }
-}
+class Sieve { 
+    /** 
+     * This main method sets n number, and prints the output
+     * of invoked sieve method.
+     */
+    public static void main(String args[]) { 
+        int n = 30; 
+        System.out.print("Following are the prime numbers "); 
+        System.out.println("smaller than or equal to " + n); 
+        Sieve g = new Sieve(); 
+        g.sieve(n); 
+    } 
+    /** 
+     * Creates a boolean array "prime[0..n]" and initialize 
+     * all entries it as true. A value in prime[i] will 
+     * finally be false if i is Not a prime, else true. 
+     */
+    void sieve(int n) { 
+        boolean prime[] = new boolean[n+1]; 
+        for(int i = 0; i < n; i++) 
+            prime[i] = true;    
+        for(int p = 2; p*p <=n; p++) 
+        { 
+            // If prime[p] is not changed, then it is a prime 
+            if(prime[p] == true) 
+            { 
+                // Update all multiples of p 
+                for(int i = p*2; i <= n; i += p) 
+                    prime[i] = false; 
+            } 
+        } 
+        
+        // Prints all prime numbers 
+        for(int i = 2; i <= n; i++) 
+        { 
+            if(prime[i] == true) 
+                System.out.print(i + " "); 
+        } 
+    } 
+} 
