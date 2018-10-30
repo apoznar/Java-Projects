955af50a2607a341d87057486fb53a736f327e96
apoznar <adampoznar@gmail.com>
2018-10-30 15:09:04 +0100

    Update ArePrimeFactors.java
    minor tweaks to the code

--- Think Java excercises/8/ArePrimeFactors.java
+++ Think Java excercises/8/ArePrimeFactors.java
@@ -1,58 +1,58 @@
 /**
- * The Hello class implements an application that 
- * displays "Hello World!" to the standard output
- * and uses some escape sequences.
+ * The ArePrimeFactors class implements an application that 
+ * takes an integer n, an array of integers and returns
+ * true if numbers in array are all prime and their product is n.
  *
  * @author  Adam Poznar
- * @version 1.1
- * @since   2018-09-11 
+ * @version 1.2
+ * @since   2018-09-18
  */
 public class ArePrimeFactors {
-     /** 
- * This main method executes both methods.
- */
+    /** 
+     * This main method executes both methods.
+     */
     public static void main(String[] args) {
+        int n = 7;
+        int[] arr = {1, 7};
+        System.out.println(arePrimeFactors(n, arr));
     }
-    
-     /** 
- * This method checks if n is > 2, then chcecks if
-* it is even.
- */
-    public static boolean isPrime(int n){
-        if (n < 2){
-            return false;
-        } 
-        else if (n > 2){
-            if (n % 2 == 0){
-                return false;
-            }
-            for (int i = 3;i<=(int)Math.sqrt(n);i+=2){
-                if (n % i !=0){
-                    return false;
-                }
-            }
-            
-        }
-        return true;
-    }
-     /** 
- * This method checks if elements in an array are..
- */
-    public static boolean arePrimeFactors(int n, int[] arr){
+    /** 
+     * This method checks if elements in an array are prime and their product is n.
+     */
+    public static boolean arePrimeFactors(int n, int[] arr) {
         int result = 1;
-        for (int element:arr){
-            if (isPrime(element)){
+        for (int element : arr) {
+            if (isPrime(element)) {
                 result *= element;
             } 
             else {
                 return false;
             }
         }
-        if (result == n){
+        if (result == n) {
             return true;
         } 
         else {
             return false;
         }
+    }
+    /** 
+     * This method checks if n is > 2, then checks if it is odd.
+     */
+    public static boolean isPrime(int n) {
+        if (n < 2) {
+            return false;
+        } 
+        else if (n > 2) {
+            if (n % 2 == 0) {
+                return false;
+            }
+            for (int i = 3; i <= (int)Math.sqrt(n); i += 2) {
+                if (n % i != 0) {
+                    return false;
+                }
+            }  
+        }
+        return true;
     }
 }
