dab730daca012a70c9891c8c528fc55e453f1f8d
apoznar <adampoznar@gmail.com>
2018-10-31 20:00:45 +0100

    Update LooplessTime.java
    major comment tweaks

--- Think Java excercises/11/LooplessTime.java
+++ Think Java excercises/11/LooplessTime.java
@@ -1,41 +1,52 @@
 /**
- * The Hello class implements an application that 
- * displays "Hello World!" to the standard output
- * and uses some escape sequences.
+ * The Loopless class implements an application that 
+ * changes increment method to one that does not use loops.
  *
  * @author  Adam Poznar
- * @version 1.1
- * @since   2018-09-15 
+ * @version 1.2
+ * @since   2018-09-21
  */
-public class LooplessTime{
- /** 
- * This main method 
- */
-   public static void main(String[] args){
-      LooplessTime time = new LooplessTime();
-      time.increment(3600*8+6*60+5);
-      System.out.println(time);
-   }
-
-   public int hour;
-   public int minute;
-   public double second;
-   public Time(int hour, int minute, double second){
-      this.hour = hour;
-      this.minute = minute;
-      this.second = second;
-   }
-   public Time(){
-      this(0,0,0);
-   }
-   public String toString(){
-      return this.hour+":"+this.minute+":"+this.second;
-   }
-   public void increment(double seconds) {
-      this.second += seconds;
-      this.minute += this.second / 60;
-      this.second %= 60;
-      this.hour += this.minute / 60;
-      this.minute %= 60;
-   }
+public class LooplessTime {
+    /** 
+     * This main method creates a new LooplessTime object called time,
+     * modify it through increment method using given argument and prints object.
+     */
+    public static void main(String[] args) {
+        LooplessTime time = new LooplessTime();
+        time.increment(3600 * 8 + 6 * 60 + 5);
+        System.out.println(time);
+    }
+    public int hour;
+    public int minute;
+    public double second;
+    /** 
+     * This constructor takes parameters and initializes the instance variables.
+     */
+    public LooplessTime(int hour, int minute, double second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+    /** 
+     * Sets parameter values to a constructor.
+     */
+    public LooplessTime() {
+        this(0, 0, 0);
+    }
+    /** 
+     * Returns instance variables in a form of a string.
+     */
+    public String toString() {
+        return this.hour + ":" + this.minute + ":" + this.second;
+    }
+    /** 
+     * Does an incrementation of time - counts final state of variables.
+     */
+    public void increment(double seconds) {
+        this.second += seconds;
+        this.minute += this.second / 60;
+        this.second %= 60;
+        this.hour += this.minute / 60;
+        this.minute %= 60;
+    }
 }
