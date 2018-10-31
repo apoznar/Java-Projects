60fd67fbe0f1bbde639b46c8dd2a82b5a90312ec
apoznar <adampoznar@gmail.com>
2018-10-31 19:45:00 +0100

    Update Date.java
    Major comment tweaks.

--- Think Java excercises/11/Date.java
+++ Think Java excercises/11/Date.java
@@ -1,29 +1,43 @@
 /**
- * The Hello class implements an application that 
- * displays "Hello World!" to the standard output
- * and uses some escape sequences.
+ * The Date class implements an application that 
+ * prints birthday date. It provides two constructors,
+ * one with no parameters and initialize default date,
+ * other take parameters and use them to initialize
+ * the instance variables.
  *
  * @author  Adam Poznar
- * @version 1.1
- * @since   2018-09-15 
+ * @version 1.2
+ * @since   2018-09-21 
  */
-public class Date{
+public class Date {
     /** 
- * This main method 
- */
-   public static void main(String args[]){
-      Date birthday = new Date();
-      System.out.println(birthday.year+" "+birthday.month+" "+birthday.day);
-   }
-   public int year;
-   public int month;
-   public int day;
-   public Date(int year, int month, int day){
-      this.year = year;
-      this.month = month;
-      this.day = day;
-   }
-   public Date(){
-      this(2001,4,17);
-   }
+     * This main method creates a new Date object called birthday
+     * and prints its instance variables in appropriate order.
+     */
+    public static void main(String args[]) {
+        Date birthday = new Date();
+        System.out.println(birthday.year + " " + birthday.month +
+                           " " + birthday.day);
+    }
+    /** 
+     * Initializing instance variables.
+     */
+    public int year;
+    public int month;
+    public int day;
+    /** 
+     * Constructor with parameters to initialize instance variables.
+     */
+    public Date(int year, int month, int day)
+    {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+    /** 
+     * Constructor with no parameters.
+     */
+    public Date() {
+        this(1991, 3, 8);
+    }
 }
