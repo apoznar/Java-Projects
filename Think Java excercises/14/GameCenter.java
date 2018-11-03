18ff561304564f593110fe1f98a174c5ba79c283
apoznar <adampoznar@gmail.com>
2018-11-03 12:11:23 +0100

    Update GameCenter.java
    Minor comment changes.

--- Think Java excercises/14/GameCenter.java
+++ Think Java excercises/14/GameCenter.java
@@ -1,25 +1,24 @@
 import java.util.ArrayList;
 /**
- * The Hello class implements an application that 
- * displays "Hello World!" to the standard output
- * and uses some escape sequences.
+ * The GameCenter class implements an application that 
+ * starts the game of Crazy Eights.
  *
  * @author  Adam Poznar
- * @version 1.1
- * @since   2018-09-15 
+ * @version 1.2
+ * @since   2018-09-24
  */
-public class GameCenter{
+public class GameCenter {
     /** 
- * This main method 
- */
-   public static void main(String args[]){
-      Eights.debug = true;
-      int count = 20;
-      ArrayList<EightsPlayer> players = new ArrayList<EightsPlayer>();
-      players.add(new EightsPlayer("Random"));
-      players.add(new EightsGenius("Genius"));
-      players.add(new EightsPlayer("Random"));
-      players.add(new EightsPlayer("Random"));
-      Eights.test(players, count);
-   }
+     * This main method adds players to the game and starts it.
+     */
+    public static void main(String args[]) {
+        Eights.debug = true;
+        int count = 20;
+        ArrayList<EightsPlayer> players = new ArrayList<EightsPlayer>();
+        players.add(new EightsPlayer("Random"));
+        players.add(new EightsGenius("Genius"));
+        players.add(new EightsPlayer("Random"));
+        players.add(new EightsPlayer("Random"));
+        Eights.test(players, count);
+    }
 }
