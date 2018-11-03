eb7c78d10b1ee417c6c945382145e1b08aac04c2
apoznar <adampoznar@gmail.com>
2018-11-03 11:56:21 +0100

    Update EightsCardCollection.java
    Minor comment changes.

--- Think Java excercises/14/EightsCardCollection.java
+++ Think Java excercises/14/EightsCardCollection.java
@@ -1,114 +1,100 @@
 import java.util.ArrayList;
 /**
- * The Hello class implements an application that 
- * displays "Hello World!" to the standard output
- * and uses some escape sequences.
+ * The EightsCardCollection class implements an application that
+ * manages deck of cards in Crazy Eights game.
  *
  * @author  Adam Poznar
- * @version 1.1
- * @since   2018-09-15 
+ * @version 1.2
+ * @since   2018-09-24 
  */
-public class EightsCardCollection extends CardCollection{
+public class EightsCardCollection extends CardCollection {
     /** 
- * This main method 
- */
-   private ArrayList<EightsCard> eightCards;
-   
+     * Invokes EightsCard class.
+     */
+    private ArrayList<EightsCard> eightCards;
     /**
      * Constructs an empty collection.
      */
-   public EightsCardCollection(String label) {
-      super(label);
-      this.eightCards = new ArrayList<EightsCard>();
-   }
-
+    public EightsCardCollection(String label) {
+        super(label);
+        this.eightCards = new ArrayList<EightsCard>();
+    }
     /**
      * Returns the card with the given index.
      */
-   public EightsCard getCardAt(int i) {
-      return eightCards.get(i);
-   }
-
+    public EightsCard getCardAt(int i) {
+        return eightCards.get(i);
+    }
     /**
      * Returns the last card.
      */
-   public EightsCard last() {
-      int i = size() - 1;
-      return getCardAt(i);
-   }
-
+    public EightsCard last() {
+        int i = size() - 1;
+        return getCardAt(i);
+    }
     /**
      * Adds the given card to the collection.
      */
-   public void append(EightsCard card) {
-      eightCards.add(card);
-   }
-
+    public void append(EightsCard card) {
+        eightCards.add(card);
+    }
     /**
      * Removes and returns the card with the given index.
      */
-   public EightsCard removeAt(int i) {
-      return eightCards.remove(i);
-   }
-
+    public EightsCard removeAt(int i) {
+        return eightCards.remove(i);
+    }
     /**
      * Removes and returns the last card.
      */
-   public EightsCard removeLast() {
-      int i = size() - 1;
-      return removeAt(i);
-   }
-
-   /**
-    * Returns the number of cards.
-    */
-   public int size() {
-      return eightCards.size();
-   }
-
+    public EightsCard removeLast() {
+        int i = size() - 1;
+        return removeAt(i);
+    }
+    /**
+     * Returns the number of cards.
+     */
+    public int size() {
+        return eightCards.size();
+    }
     /**
      * Moves n cards from this collection to the given collection.
      */
-   public void deal(EightsCardCollection that, int n) {
-      for (int i = 0; i < n; i++) {
-         EightsCard card = removeLast();
-         that.append(card);
-      }
-   }
-
+    public void deal(EightsCardCollection that, int n) {
+        for (int i = 0; i < n; i++) {
+            EightsCard card = removeLast();
+            that.append(card);
+        }
+    }
     /**
      * Moves all remaining cards to the given collection.
      */
-   public void dealAll(EightsCardCollection that) {
-      int n = size();
-      deal(that, n);
-   }
-
+    public void dealAll(EightsCardCollection that) {
+        int n = size();
+        deal(that, n);
+    }
     /**
-    * Swaps the cards at indexes i and j.
-    */
-   public void swapAt(int i, int j) {
-      append(eightCards.get(i));
-      eightCards.set(i, getCardAt(j));
-      eightCards.set(j, removeLast());
-   }
-
-   /**
-    * Returns a string representation of the card collection.
-    */
-   public String toString() {
-      return getLabel() + ": " + eightCards.toString();
-   }
-
-   /**
-    * Prints the label and cards.
-    */
-   public void display() {
-      System.out.println(getLabel() + ": ");
-      for (Card card: eightCards){
-         System.out.println(card);
-      }
-      System.out.println();
-   }
-
+     * Swaps the cards at indexes i and j.
+     */
+    public void swapAt(int i, int j) {
+        append(eightCards.get(i));
+        eightCards.set(i, getCardAt(j));
+        eightCards.set(j, removeLast());
+    }
+    /**
+     * Returns a string representation of the card collection.
+     */
+    public String toString() {
+        return getLabel() + ": " + eightCards.toString();
+    }
+    /**
+     * Prints the label and cards.
+     */
+    public void display() {
+        System.out.println(getLabel() + ": ");
+        for (Card card: eightCards){
+            System.out.println(card);
+        }
+        System.out.println();
+    }
 }
