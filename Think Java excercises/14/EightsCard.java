6f795176e8fddfa4ff2b60a87781b2f51b8034d5
apoznar <adampoznar@gmail.com>
2018-11-03 11:53:22 +0100

    Update EightsCard.java
    Minor comment changes.

--- Think Java excercises/14/EightsCard.java
+++ Think Java excercises/14/EightsCard.java
@@ -1,50 +1,48 @@
 /**
- * The Hello class implements an application that 
- * displays "Hello World!" to the standard output
- * and uses some escape sequences.
+ * The EightsCard class implements an application that 
+ * returns score of cards in a game of Crazy Eights.
+ * 
  *
  * @author  Adam Poznar
- * @version 1.1
- * @since   2018-09-15 
+ * @version 1.2
+ * @since   2018-09-24
  */
-public class EightsCard extends Card{
- /** 
- * This main method 
- */
-   public EightsCard(int rank, int suit) {
-      super(rank, suit);
-   }
-    
-   /**
-    * Returns `true` if matches `that` with rule of crazy eights
-    * Otherwise `false`
-   */
-   public boolean matches(EightsCard that){
-      if (getSuit() == that.getSuit()) {
-         return true;
-      }
-      if (getRank() == that.getRank()) {
-         return true;
-      }
-      if (getRank() == 8) {
-         return true;
-      }
-      return false;
-   }
-
-   /**
-    * Returns the score of the card
-   */
-   public int score(){
-      int rank = getRank();
-      if (rank == 8) {
-         return -20;
-      } 
-      else if (rank > 10) {
-         return -10;
-      } 
-      else {
-         return -rank;
-      }
-   }
+public class EightsCard extends Card {
+    /** 
+     * Invokes super method with given parameters.
+     */
+    public EightsCard(int rank, int suit) {
+        super(rank, suit);
+    }
+    /**
+     * Returns `true` if matches `that` with rule of crazy eights
+     * Otherwise `false`.
+     */
+    public boolean matches(EightsCard that) {
+        if (getSuit() == that.getSuit()) {
+            return true;
+        }
+        if (getRank() == that.getRank()) {
+            return true;
+        }
+        if (getRank() == 8) {
+            return true;
+        }
+        return false;
+    }
+    /**
+     * Returns the score of the card.
+     */
+    public int score() {
+        int rank = getRank();
+        if (rank == 8) {
+            return -20;
+        } 
+        else if (rank > 10) {
+            return -10;
+        } 
+        else {
+            return -rank;
+        }
+    }
 }
