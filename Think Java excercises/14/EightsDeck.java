a935b3af950dbfef8ab6b6915f116a1de8bccf05
apoznar <adampoznar@gmail.com>
2018-11-03 11:59:17 +0100

    Update EightsDeck.java
    Comment changes.

--- Think Java excercises/14/EightsDeck.java
+++ Think Java excercises/14/EightsDeck.java
@@ -1,31 +1,27 @@
 /**
- * The Hello class implements an application that 
- * displays "Hello World!" to the standard output
- * and uses some escape sequences.
+ * The EightsDeck class implements an application that
+ * construct a deck for Crazy Eights game.
  *
  * @author  Adam Poznar
- * @version 1.1
- * @since   2018-09-15 
- */
-/**
- * A deck of playing cards.
+ * @version 1.2
+ * @since   2018-09-24 
  */
 public class EightsDeck extends EightsCardCollection {
-
-   public EightsDeck(String label) {
-      super(label);
-      initialize();
-   }
-
-   /**
-    * Constructs a standard deck of 52 cards.
-    */
-   private void initialize(){
-      for (int suit = 0; suit <= 3; suit++) {
-         for (int rank = 1; rank <= 13; rank++) {
-            append(new EightsCard(rank, suit));
-         }
-      }
-   }
-
+    /**
+     * Invokes methods that construct deck of cards.
+     */
+    public EightsDeck(String label) {
+        super(label);
+        initialize();
+    }
+    /**
+     * Constructs a standard deck of 52 cards.
+     */
+    private void initialize() {
+        for (int suit = 0; suit <= 3; suit++) {
+            for (int rank = 1; rank <= 13; rank++) {
+                append(new EightsCard(rank, suit));
+            }
+        }
+    }
 }
