0749be790ee9200a3f51f5315806f13261bd2763
apoznar <adampoznar@gmail.com>
2018-11-05 12:02:32 +0100

    Update JapaneseFlag.java
    Small comment changes

--- Think Java excercises/Appendix B/JapaneseFlag.java
+++ Think Java excercises/Appendix B/JapaneseFlag.java
@@ -1,27 +1,32 @@
 import java.awt.*;
 import javax.swing.*;
 /**
- * The Hello class implements an application that 
- * displays "Hello World!" to the standard output
- * and uses some escape sequences.
- *
+ * The JapaneseFlag class implements an application that 
+ * creates a canvas that recreates japanese national flag.
+ * 
  * @author  Adam Poznar
- * @version 1.1
- * @since   2018-09-15 
+ * @version 1.2
+ * @since   2018-09-25
  */
-public class JapaneseFlag extends Canvas{
-   public static void main(String[] args){
-      JFrame frame = new JFrame("Window Title");
-      Canvas canvas = new JapaneseFlag();
-      canvas.setSize(800, 400);
-      canvas.setBackground(Color.white);
-      frame.add(canvas);
-      frame.pack();
-      frame.setVisible(true);
-   }
-   public void paint(Graphics g){
-      // Drawing codes
-      g.setColor(Color.red);
-      g.fillOval(250,50,300,300);
-   }
+public class JapaneseFlag extends Canvas {
+    /** 
+     * This main method creates a new JFrame object,
+     * sets size of canvas and its white color.
+     */
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Window Title");
+        Canvas canvas = new JapaneseFlag();
+        canvas.setSize(800, 400);
+        canvas.setBackground(Color.white);
+        frame.add(canvas);
+        frame.pack();
+        frame.setVisible(true);
+    }
+    /** 
+     * Creates a red circle on canvas.
+     */
+    public void paint(Graphics g) {
+        g.setColor(Color.red);
+        g.fillOval(250,50,300,300);
+    }
 }
